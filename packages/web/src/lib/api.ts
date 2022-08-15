7f1300003fcea7d78416ce23e52f848c861711ad
Bruno Alves <bruno@sthorm.io>
2022-08-15 18:26:49 -0300

    feat: add link to url server

--- packages/web/src/lib/api.ts
+++ packages/web/src/lib/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://localhost:3000'
+const BASE_URL = import.meta.env.VITE_BASE_URL ?? 'http://localhost:3333'
+console.log(import.meta.env.VITE_BASE_URL)
 export const api = axios.create({
   baseURL: BASE_URL,
 })
